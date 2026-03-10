cc9e17c9b50d112a787a6c496ccfc0cf549a4b87
Grizzz88 <mich.griz.ch@gmail.com>
2026-03-10 00:47:53 -0400

    feat: mostrar 15-Love cuando jugador 1 anota

--- src/tennis.js
+++ src/tennis.js
@@ -2,6 +2,9 @@ function marcador(p1, p2) {
   if (p1 === 0 && p2 === 0) {
     return 'Love-Love';
   }
+  if (p1 === 1 && p2 === 0) {
+    return "15-Love";
+  }
 }
 
 export default marcador;
--- src/tennis.test.js
+++ src/tennis.test.js
@@ -3,3 +3,7 @@ import marcador from './tennis.js';
 test('empieza el set', () => {
   expect(marcador(0, 0)).toBe('Love-Love');
 });
+
+test('jugador 1 anota', () => {
+  expect(marcador(1,0)).toBe('15-Love');
+});
